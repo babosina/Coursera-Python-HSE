4bccb61e8216bb1f11db69a4313253dd819e966d
babosina <babos77@gmail.com>
2017-06-27 22:00:49 +0300

    Exercises

---
+++ Week_1/ex2.py
@@ -0,0 +1,14 @@
+ping = """
+   _~_    
+  (o o)   
+ /  V  \  
+/(  _  )\ 
+  ^^ ^^
+"""
+
+number = int(input())
+print('_~_' * number, ' ' * number, end=' \n')
+print('(0 0)' * number, end=' \n')
+print('/  V  \\' * number, end=' \n')
+print('/(  _  )\\' * number, end=' \n')
+print('  ^^ ^^  ' * number, end=' ')
